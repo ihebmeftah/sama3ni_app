c10520ef7c9ce9e51e2fcf6e1bc5356f6f34a87e
ihebmeftah <ihebmeftah666@gmail.com>
2024-10-26 11:41:09 +0100

    clear

--- sama3ni_flutter/lib/app/modules/kits/controllers/kits_controller.dart
+++ sama3ni_flutter/lib/app/modules/kits/controllers/kits_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class KitsController extends GetxController {
-  //TODO: Implement KitsController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class KitsController extends GetxController {}
--- sama3ni_flutter/lib/app/modules/music/controllers/music_controller.dart
+++ sama3ni_flutter/lib/app/modules/music/controllers/music_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class MusicController extends GetxController {
-  //TODO: Implement MusicController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class MusicController extends GetxController {}

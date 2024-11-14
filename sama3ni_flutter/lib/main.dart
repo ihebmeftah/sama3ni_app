2bf88f22a059787c2e3619efbb0782341a5a4448
ihebmeftah <ihebmeftah666@gmail.com>
2024-11-14 23:06:26 +0100

    Refactor main.dart to use a StatelessWidget for the App
    Encapsulate the GetMaterialApp widget within a new StatelessWidget called App to improve code organization and readability. This change makes the main function cleaner and separates the app configuration into its own class.

--- sama3ni_flutter/lib/main.dart
+++ sama3ni_flutter/lib/main.dart
@@ -8,18 +8,27 @@ import 'package:sama3ni_flutter/app/config/appthemes.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-GetMaterialApp(
-        title: "Sama3ni",
-        theme: AppThemes.light,
-        darkTheme: AppThemes.dark,
-        themeMode: ThemeMode.dark,
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.HOME,
-        getPages: AppPages.routes,
-        defaultTransition: Transition.fade,
-        initialBinding: NavbarBinding(),
-        builder: (_, c) => NavbarView(child: c!),
-      )
-  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Sama3ni",
+      theme: AppThemes.light,
+      darkTheme: AppThemes.dark,
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.HOME,
+      getPages: AppPages.routes,
+      defaultTransition: Transition.fade,
+      initialBinding: NavbarBinding(),
+      builder: (_, c) => NavbarView(child: c!),
+    );
+  }
 }

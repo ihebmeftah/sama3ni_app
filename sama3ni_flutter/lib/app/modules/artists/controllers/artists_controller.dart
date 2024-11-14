97c579c14094c87869206a4c54a2db181fa49f0d
ihebmeftah <ihebmeftah666@gmail.com>
2024-11-14 23:11:18 +0100

    Remove unnecessary methods and properties from ArtistsController
    Simplified the ArtistsController by removing the unused count variable and its associated methods. The controller now only extends GetxController without any additional functionality.

--- sama3ni_flutter/lib/app/modules/artists/controllers/artists_controller.dart
+++ sama3ni_flutter/lib/app/modules/artists/controllers/artists_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class ArtistsController extends GetxController {
-  //TODO: Implement ArtistsController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class ArtistsController extends GetxController {}

bbcffa8477377775fb98b82c2db3be15ccd9b0a0
ihebmeftah <ihebmeftah666@gmail.com>
2024-12-22 20:15:18 +0100

    old work

--- sama3ni_flutter/lib/app/modules/artists/controllers/artists_profile_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class ArtistsProfileController extends GetxController {
-  final id = Get.parameters["id"];
-}
--- sama3ni_server/bin/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:sama3ni_server/server.dart';
-
-/// This is the starting point for your Serverpod server. Typically, there is
-/// no need to modify this file.
-void main(List<String> args) {
-  run(args);
-}

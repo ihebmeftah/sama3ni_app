97c1cee51f9f3465386ded765c9a2c417e353feb
ihebmeftah <ihebmeftah666@gmail.com>
2025-01-03 16:14:04 +0100

    center

--- sama3ni_flutter/lib/app/components/appempty.dart
+++ sama3ni_flutter/lib/app/components/appempty.dart
@@ -7,6 +7,8 @@ class AppEmpty extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         spacing: 20,
         children: [
           Image.asset("assets/images/iconempty.png"),
